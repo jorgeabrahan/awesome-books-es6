fad30c77c14fe0e464ae89da6020d509e81179e1
Jorge <jorge24abrahan@gmail.com>
2023-01-16 10:48:22 -0600

    Show success/error message when submitting the add-book form

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import Books from './modules/Books.js';
 
 const booksCnt = document.getElementById('booksCnt');
 const frmAddBook = document.getElementById('frmAddBook');
+const frmAddMsg = document.getElementById('frmAddMsg');
 const btnsPages = document.querySelectorAll('button[to-open]');
 
 btnsPages.forEach((btnPage) => {
@@ -18,11 +19,30 @@ btnsPages.forEach((btnPage) => {
 
 const books = new Books(booksCnt);
 
+const showFrmAddBookMsg = (isSuccess, msg) => {
+  frmAddMsg.classList.remove('d-none');
+  if (isSuccess) {
+    frmAddMsg.classList.add('message--success');
+    setTimeout(() => {
+      frmAddMsg.className = 'd-none message';
+    }, 2500);
+  } else {
+    frmAddMsg.classList.add('message--error');
+  }
+  frmAddMsg.textContent = msg;
+};
+
 frmAddBook.addEventListener('submit', (e) => {
   e.preventDefault();
+  frmAddMsg.className = 'd-none message';
   const title = frmAddBook.title.value.trim();
   const author = frmAddBook.author.value.trim();
+  if (title.length === 0 || author.length === 0) {
+    showFrmAddBookMsg(false, 'Fill all inputs before submiting!');
+    return;
+  }
   books.add(title, author);
   frmAddBook.reset();
   frmAddBook.title.focus();
+  showFrmAddBookMsg(true, 'Book added successfully!');
 });
